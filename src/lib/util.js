cb21e425c775d20ffbab1547aad81e967aa15825
InkoHX <inkohx@gmail.com>
2020-08-22 23:09:29 +0900

    fix(Util): searchGitHubHighlightedLineLinks regex

--- src/lib/util.js
+++ src/lib/util.js
@@ -12,4 +12,4 @@ module.exports.searchCodeBlocks = str => str.matchAll(/`{3}(?<extension>[\w]+)?\
  * @param {string} str
  * @returns {IterableIterator<RegExpMatchArray>}
  */
-module.exports.searchGitHubHighlightedLineLinks = str => str.matchAll(/https?:\/\/github\.com\/(?<owner>.*)\/(?<repo>.*)\/blob\/(?<branch>.*?)\/(?<path>.*)#L(?<firstLine>[0-9]+)-?L?(?<lastLine>[0-9]+)?/gu)
+module.exports.searchGitHubHighlightedLineLinks = str => str.matchAll(/https?:\/\/github\.com\/(?<owner>[\w\W]{1,39})\/(?<repo>[\w\W]{1,100})\/blob\/(?<branch>[\w\W]{1,248}?)\/(?<path>.+)#L(?<firstLine>[0-9]+)-?L?(?<lastLine>[0-9]+)?/gu)
