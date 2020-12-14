27cb81e613432e5df0db0bf73caa363408d2f3ad
InkoHX <me@inkohx.dev>
2020-12-15 00:17:24 +0900

    fix #65

--- src/command/scrapbox.js
+++ src/command/scrapbox.js
@@ -57,8 +57,8 @@ module.exports = new CommandBuilder()
         .setTitle(page.title)
         .setURL(`https://scrapbox.io/${project}/${encodeURIComponent(page.title)}`)
         .setThumbnail('https://i.gyazo.com/7057219f5b20ca8afd122945b72453d3.png')
-        .setDescription(page.snipet
-          .map(text => text.replace(/<b>|<\/b>/g, '**'))
+        .setDescription(page.lines
+          .map(text => text.trim())
           .join('\n')
         )
         .addField('閲覧数', page.views, true)
